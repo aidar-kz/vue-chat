4d87d966487a113b5a5448b1223631404d41b05a
Aydeke <aydeke2@gmail.com>
2022-05-27 20:14:12 +0600

    Add data, methods, listeners

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,55 @@
 <template>
-  <h1>Vue chat</h1>
+  <main class="container p-5"></main>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: [],
+      messages: [],
+      username: "",
+      selected: 0,
+      isReady: false,
+      isTyping: false,
+      userId: "",
+      errorLoginMsg: "",
+    };
+  },
+  methods: {
+    joinToChat() {
+      if (!this.username) {
+        this.errorLoginMsg = "Введите имя пользователя";
+      } else {
+        this.$socket.emit("enterUsername", { username: this.username });
+        this.username = "";
+        this.isReady = true;
+        this.userId = this.$socket.id;
+      }
+    },
+  },
+  mounted() {
+    this.$socket.on("initChat", (data) => {
+      this.messages = data.messages;
+    });
+
+    this.$socket.on("getUsers", (data) => {
+      this.users = data;
+    });
+
+    this.$socket.on("newMessage", (data) => {
+      this.messages.push(data);
+    });
+
+    this.$socket.on("isTyping", (data) => {
+      this.isTyping = data;
+    });
+
+    this.$socket.on("stopTyping", () => {
+      this.isTyping = false;
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
